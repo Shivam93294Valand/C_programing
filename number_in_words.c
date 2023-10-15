51c567103515d76aff0d983c03f179849b8b404c
Shivam93294Valand <147402134+Shivam93294Valand@users.noreply.github.com>
2023-10-15 17:26:19 +0530

    Add files via upload

---
+++ number_in_words.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int main()
+{
+    int n, digit, reversed = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n /= 10;
+    }
+
+    while (reversed != 0) 
+    {
+        digit = reversed % 10;
+        switch (digit) 
+        {
+            case 0:
+                printf("Zero ");
+                break;
+            case 1:
+                printf("One ");
+                break;
+            case 2:
+                printf("Two ");
+                break;
+            case 3:
+                printf("Three ");
+                break;
+            case 4:
+                printf("Four ");
+                break;
+            case 5:
+                printf("Five ");
+                break;
+            case 6:
+                printf("Six ");
+                break;
+            case 7:
+                printf("Seven ");
+                break;
+            case 8:
+                printf("Eight ");
+                break;
+            case 9:
+                printf("Nine ");
+                break;
+            default:
+                printf("Invalid input\n");
+                return 1;
+        }
+        reversed /= 10;
+    }
+    return 0;
+}
